7b6df8bf90616e7fb980c9bf3450dc2a1621ef72
victorteesinloong <victortee89@hotmail.com>
2014-12-18 18:43:56 +0900

    change kmeans

--- samples/pc/kmeans/test02/kmeans.cpp
+++ samples/pc/kmeans/test02/kmeans.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 #define NUM 10
+#define cNUM 3
 
 int GetRandom(int min,int max)
 {
@@ -12,8 +13,15 @@ int GetRandom(int min,int max)
 
 int main()
 {
+
 	srand((unsigned int)time(NULL));
 	for(int i=0; i<NUM; i++){
 		cout << GetRandom(0,100) << endl;
-	}
+		}
+
+	cout << endl;
+
+	for(int j=0; j<cNUM; j++){
+			cout<<"center="<< GetRandom(0,100) << endl;
+		}
 }
