05b5c627335c34885ec54ccb7a54d711876ad2e0
victorteesinloong <victortee89@hotmail.com>
2014-12-18 19:30:46 +0900

    change kmeans

--- samples/pc/kmeans/test02/kmeans.cpp
+++ samples/pc/kmeans/test02/kmeans.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <time.h>
+#include <windows.h>
 
 using namespace std;
 
@@ -13,15 +14,17 @@ int GetRandom(int min,int max)
 
 int main()
 {
-
+	int num[10];
 	srand((unsigned int)time(NULL));
 	for(int i=0; i<NUM; i++){
-		cout << GetRandom(0,100) << endl;
+		num[i]=i+1;
+		cout <<"num["<<num[i]<<"]"<<"="<< GetRandom(0,100) << endl;
 		}
 
 	cout << endl;
 
 	for(int j=0; j<cNUM; j++){
-			cout<<"center="<< GetRandom(0,100) << endl;
+		num[j]=j+1;
+			cout<<"num["<<num[j]<<"]"<<"="<< GetRandom(0,100) << endl;
 		}
 }
