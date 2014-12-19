181aac9437c6dbfa8a83c975b418f9ead10fa08d
victorteesinloong <victortee89@hotmail.com>
2014-12-19 18:05:48 +0900

    change kmeans

--- samples/pc/kmeans/test02/kmeans.cpp
+++ samples/pc/kmeans/test02/kmeans.cpp
@@ -1,90 +1,40 @@
-#include<iostream>
-#include<iomanip>
-#include<algorithm>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <time.h>
+#include <windows.h>
 
 using namespace std;
 
-#define kcount 3
-#define data 10
+#define NUM 10
+#define cNUM 3
 
-class pos{
-public:
-	double enc;
-};
+int GetRandom(int min,int max)
+{
+	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
+}
 
 int main()
 {
-	pos* k=new pos[kcount];
-	pos* center=new pos[kcount];
+	int num[10], cnum[3];
+	srand((unsigned int)time(NULL));
+	for(int i=0; i<NUM; i++){
+		num[i]=GetRandom(0,100);
+		cout<<"No["<<i<<"] = "<<num[i]<<endl;
+	}
 
-	double count_group[kcount]={};
+	cout << endl;
 
-	vector<pos>datas;
-	vector<double> distance[kcount];
-
-	for(int i=0;i<data;i++){
-        pos tmp;
-		tmp.enc = (double)(rand()%100);
-		cout<<"("<<tmp.enc<<")"<<endl;
-        datas.push_back(tmp);
-    }
-	
-	for(int i=0; i<kcount; i++)
-    {
-        k[i] = datas[i];
-        center[i].enc = datas[i].enc;
-		distance[i].resize(data);
-    }
-    bool loop = true;
-    while(loop){ //when the k-positions are all same with next position.
-       
-        //center init
-		for(int i=0;i<kcount; i++){
-			center[i].enc= (double)(rand()%100);
-            count_group[i] =(double)(rand()%100);
-        }
-
-		// distance
-        for(int i=0;i<datas.size(); i++){
-			for(int j=0;j<kcount; j++){
-                double tmp_distance = sqrt ( pow( k[j].enc - datas[i].enc, 2));
-					distance[j][i] = tmp_distance;
-            }
-        }
- 
-        //get center
-        for(int i=0;i<datas.size(); i++){
-            double min = distance[0][i];
-            int min_j = 0;
- 
-			for(int j=1;j<kcount; j++){
-                if(min > distance[j][i]){
-                    min = distance[j][i];
-                    min_j = j;
-                }
-            }
-			center[ min_j ].enc += datas[i].enc;
-            count_group[min_j]++;
-        }
- 
-        //change K
-        int same_count = 0;
-		for(int i=0;i<kcount; i++){
-           
- 
-            if(count_group[i] != 0){
-                if((center[i].enc / count_group[i]) == k[i].enc)
-                        same_count ++;
-               
-                k[i].enc= center[i].enc / count_group[i];
-            }
-			if(same_count == kcount){
-                loop=false;
-            }
-            cout<<fixed<<setprecision(2);
-			cout<<"("<<k[i].enc<<") ";
+	for(int j=0; j<cNUM; j++){
+		cnum[j]=GetRandom(0,100);
+			cout<<"centerNo["<<j<<"]="<<cnum[j]<< endl;
 		}
-        }cout<<endl;
-    }//end of loop
+
+	cout<<endl;
+	
+	for(int i=0; i<NUM; i++){
+		for(int j=0; j<cNUM; j++){
+			int result=abs(num[i]-cnum[j]);
+			cout<<"result["<<i<<"]["<<j<<"]="<<result<<endl;
+			
+		}
+	}
+}
