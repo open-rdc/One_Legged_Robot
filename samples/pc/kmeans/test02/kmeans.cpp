87e3ec84ba63cd1aed9238e6c0adf69069554980
Kohei Izumi <kohei.izumi@gmail.com>
2014-12-24 18:25:01 +0900

    Add export .csv file.

--- samples/pc/kmeans/test02/kmeans.cpp
+++ samples/pc/kmeans/test02/kmeans.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <time.h>
 
 using namespace std;
@@ -17,6 +18,8 @@ int main()
 	int num[NUM], cnum[cNUM];
 	int result[NUM][cNUM];
 	srand((unsigned int)time(NULL));
+
+	ofstream ofs("test.csv");
 
 	for(int i=0; i<NUM; i++){
 		num[i]=GetRandom(0,100);
@@ -41,19 +44,32 @@ int main()
 	}
 	cout<<endl;
 
-	int min[10];
+	int min[10][2];
 	for(int i=0; i<NUM; i++){
-		min[i] = result[i][0];
+		min[i][0] = result[i][0];
+		min[i][1] = 0;
 		for(int j=0; j<cNUM; j++){
-			if(result[i][j]<min[i]){
-				min[i]=result[i][j];
+			if(result[i][j]<min[i][0]){
+				min[i][0]=result[i][j];
+				min[i][1]=j;
 			}
 		}
-		cout<<"min ["<<i<<"]: "<<min[i]<< endl;
+		cout<<"min ["<<i<<"][0]: "<<min[i][0]<<endl;
+		cout<<"min[" <<i<<"][1]: "<<min[i][1]<<endl;
 	}
+	
+	for(int i=0; i<NUM; i++)
+	{
+		cout<<"----- "<<i<<"番目の個体 -----"<<endl;
+		//ofs << "No." << i << endl;
+		cout<<"個体の数値: "<<num[i]<<endl;
+		cout<<"センターポイントの位置"<<cnum[min[i][1]]<<endl;
+		cout<<"センターポイントの距離: "<<min[i][0]<<endl;
+		cout<<"所属するセンターポイント: "<<min[i][1]<<endl;
+		ofs << num[i] << "\t" <<cnum[min[i][1]]<<"\t"<<min[i][0] <<"\t"<< min[i][1]<<endl;
+	}
+
 
 	cout<<endl;		
 
 }
-
-	
