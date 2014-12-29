97022bcc98ce0f893100a5c75b4ce5754f68828f
victorteesinloong <victortee89@hotmail.com>
2014-12-29 14:41:13 +0900

    change kmeans

--- samples/pc/kmeans/test02/kmeans.cpp
+++ samples/pc/kmeans/test02/kmeans.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <time.h>
+#include <bitset>
 
 using namespace std;
 
@@ -13,20 +12,16 @@ int GetRandom(int min,int max)
 	return min + (int)(rand()*(max-min+1.0)/(1.0+RAND_MAX));
 }
 
-
 int main()
 {
 	int num[NUM], cnum[cNUM];
 	int result[NUM][cNUM];
 	srand((unsigned int)time(NULL));
-
-	ofstream ofs(std::to_string((unsigned int)time(NULL)) + ".csv");
-
+	
 	for(int i=0; i<NUM; i++){
 		num[i]=GetRandom(0,100);
 		cout<<"No["<<i<<"] = "<<num[i]<<endl;
 	}
-
 	cout << endl;
 
 	for(int j=0; j<cNUM; j++){
@@ -35,7 +30,11 @@ int main()
 		}
 
 	cout<<endl;
-	
+
+	bool loop=true;
+	while (loop)
+	{
+
 	for(int i=0; i<NUM; i++){
 		for(int j=0; j<cNUM; j++){
 			result[i][j]=abs(num[i]-cnum[j]);
@@ -43,45 +42,68 @@ int main()
 				
 		}
 	}
+
 	cout<<endl;
 
-	int min[NUM][2];
+	int min[NUM][cNUM];
 	for(int i=0; i<NUM; i++){
 		min[i][0] = result[i][0];
-		min[i][1] = 0;
+		min[i][1]=0;
 		for(int j=0; j<cNUM; j++){
 			if(result[i][j]<min[i][0]){
 				min[i][0]=result[i][j];
 				min[i][1]=j;
 			}
+			
 		}
-		cout<<"min ["<<i<<"][0]: "<<min[i][0]<<endl;
-		cout<<"min[" <<i<<"][1]: "<<min[i][1]<<endl;
 	}
 
-	int point_counter[cNUM]={};
-	for(int i=0;i<NUM;i++)
-	{
-		for(int j=0;j<cNUM;j++)
-		{
-			if(min[i][1]==j)
-			{
-				point_counter[j]++;
-			}
-		}
-	}
+	cout<<endl;
 
 	for(int i=0; i<NUM; i++)
 	{
 		cout<<"----- "<<i<<"番目の個体 -----"<<endl;
-		cout<<"個体の数値: "<<num[i]<<endl;
-		cout<<"センターポイントの位置"<<cnum[min[i][1]]<<endl;
-		cout<<"センターポイントの距離: "<<min[i][0]<<endl;
 		cout<<"所属するセンターポイント: "<<min[i][1]<<endl;
-		ofs<<num[i]<<"\t"<<cnum[min[i][1]]<<"\t"<<min[i][0] <<"\t"<<min[i][1]<<endl;
+		cout<<"個体の数値: "<<num[i]<<endl;
+		cout<<"センターポイント："<<cnum[min[i][1]]<<endl;
+		cout<<"センターポイントとの距離: "<<min[i][0]<<endl;
+		cout<<endl;
+			
 	}
-	for(int i=0;i<cNUM;i++)
-	{
-		cout<<i<<"番目のセンターポイントに所属する個数: "<<point_counter[i]<<endl;
+	
+	cout<<endl;	
+
+	int a[cNUM]={};
+	int sum[cNUM]={};
+	int c[NUM]={};
+	int same_count=0;
+	for(int i=0; i<NUM; i++){
+		for(int j=0; j<cNUM; j++){
+			if(min[i][1]==j){
+				a[j]++;
+				sum[j] += num[i];
+				c[j]=sum[j]/cnum[j];
+			}
+			if(same_count==j){
+				loop=false;
+			}
+		}
+		
 	}
+	
+	
+	for(int j=0; j<cNUM; j++){
+		cout<<j<<"番目のセンターポイントに所属する個体数："<<a[j]<<endl;
+		cout<<"個体の合計"<<sum[j]<<endl;		
+	}
+	
+	cout<<endl;
+
+	for(int j=0; j<cNUM; j++){
+		cout<<"新しい中心点: "<<c[j]<<endl;
+	}
+	}
+	
 }
+
+	
