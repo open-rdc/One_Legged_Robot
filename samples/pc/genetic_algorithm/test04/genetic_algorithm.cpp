1593a3ba5d2fdabd03bdc487ac749ca061ead869
victorteesinloong <victortee89@hotmail.com>
2014-12-31 14:20:57 +0900

    change genetic algorithm.cpp and parameter.h

--- samples/pc/genetic_algorithm/test04/genetic_algorithm.cpp
+++ samples/pc/genetic_algorithm/test04/genetic_algorithm.cpp
@@ -8,35 +8,23 @@
 #include <boost/date_time/c_local_time_adjustor.hpp>
 #include <boost/chrono.hpp>
 #include <boost/thread.hpp>
-#include "parameter.h" 
-#include "individualData.h"
-
-int count =0;
-int count2 =0;
-double	centralPoint[10];		// クラスタセントラルポイント座標
-int		population[10];		// クラスタに属する個体数
-individualData inv[RANDOM_MAX];
-individualData invAll[RANDOM_MAX];
+#include "parameter.h"
 
 unsigned long long GetTimeStamp()
 {
-	//現在時刻のTimeStamp取得
 	namespace pt = boost::posix_time;
 	namespace gg = boost::gregorian;
 
 	typedef boost::date_time::c_local_adjustor<pt::ptime> local_adj;
 
-	// 1970/1/1 からの差分を取得
 	auto epoch = local_adj::utc_to_local(pt::ptime(gg::date(1970, 1, 1)));
 	auto diff = pt::microsec_clock::local_time() - epoch;
 
-	// 差分を秒で表示
 	return diff.total_microseconds();
 }
 
 std::string GetTimeISOString()
 {
-	//get current time to milliseconds
 	boost::posix_time::ptime today = boost::posix_time::second_clock::local_time();
 
 	return boost::posix_time::to_iso_string(today);
@@ -44,13 +32,25 @@ std::string GetTimeISOString()
 
 void Random(int min, int max, int result[RANDOM_MAX])
 {
-	//乱数生成
 	boost::random::mt19937 gen(GetTimeStamp());
-	boost::random::uniform_int_distribution<> dist(min, max);//乱数分布
+	boost::random::uniform_int_distribution<> dist(min, max);
 
 	for(int i=0; i<RANDOM_MAX; i++)
 	{
 		result[i] = dist(gen);
+	}
+
+	
+}
+
+void CenterRandom(int min, int max, int centerpoint[cNUM])
+{
+	boost::random::mt19937 gen(GetTimeStamp());
+	boost::random::uniform_int_distribution<> dist(min, max);
+
+	for(int j=0; j<cNUM; j++)
+	{
+		centerpoint[j]=dist(gen);
 	}
 }
 
@@ -58,70 +58,73 @@ int Random(int min, int max)
 {
 	boost::random::mt19937 gen(GetTimeStamp());
 	boost::random::uniform_int_distribution<> dist(min, max);
-	//sleep for milliseconds
+
 	boost::this_thread::sleep(boost::posix_time::microseconds(1000));
-	
+
 	return dist(gen);
 }
+
 
 void Initialize(int angle[])
 {
 	Random(0, 180, angle);
-}
-
-void initCentralPoint()
-{
-	for(int i=0;i<10;i++){
-		centralPoint[i]=-180 + (double)(2*180)/10 * i;
-	}
-}
-
-void clustering(int angle[])
-{
 	
-	// 距離
-	int dist[2]={0,0};
-	//個体をクラスタに分類
+}
 
-	for(int loop=0;loop<5;loop++){
-		// 各個体に対して
-		for(int i=0;i<RANDOM_MAX;i++){
-			dist[1]=180;//sinとcosの角度
-			// 各クラスタに対して
-			for(int j=0;j<10;j++){
-				// 個体とクラスタの基準線との距離を計算
-				dist[0]=fabs(centralPoint[j] - inv[i].getGene()); //fabs絶対値を表す
-				// 今まで一番近かったクラスタより距離が近かったら
-				if(dist[0]<dist[1]){//rangeの範囲以外はクラスタに設定しない
-					// 現在の個体をそのクラスタに設定
-					inv[i].setCluster(j);
-					dist[1]=dist[0];
-				}//0～180の範囲で設定、中心点と
+void CenterInitialize(int centerpoint[])
+{
+	CenterRandom(0, 180, centerpoint);
+}
+
+void clustering(int angle[], int centerpoint[]){
+	int distance[RANDOM_MAX][cNUM];
+	int min[RANDOM_MAX][cNUM];
+	int point_counter[cNUM]={};
+	int individualsum[cNUM]={};
+	int same_count = 0;
+	bool loop=true;
+
+	while (loop)
+	{
+		for(int i=0; i<RANDOM_MAX; i++){
+			for(int j=0; j<cNUM; j++){
+				distance[i][j]=abs(angle[i]-centerpoint[j]);	
 			}
 		}
-	}
-	//クラスタごとの個体数を計算
-	for(int i=0;i<10;i++) population[i]=0;				//初期化
-	for(int i=0;i<10;i++)  population[inv[i].getCluster()]++;
 
-
-	//クラスタのセンターポイントを計算
-	double sum[10];
-	for(int i=0;i<10;i++) sum[i]=0;						//初期化	
-
-	// クラスタごとの全固体の解空間の値を合計
-	for(int i=0;i<RANDOM_MAX;i++){
-		sum[inv[i].getCluster()] += inv[i].getGene();
-	}
-	//平均を取る
-	for(int i=0;i<10;i++){
-		if(population[i]!=0){
-			sum[i] /= population[i];
-			centralPoint[i] = sum[i];
+		for(int i=0; i<RANDOM_MAX; i++){
+			min[i][0] = distance[i][0];
+			min[i][1]=0;
+			for(int j=0; j<cNUM; j++){
+				if(distance[i][j]<min[i][0]){
+					min[i][0]=distance[i][j];
+					min[i][1]=j;
+				}
+			}
 		}
-		// クラスタに属する個体が無くなった場合
-		// → 新しいクラスタをランダムに作成
-		else centralPoint[i]=(double)rand()/RAND_MAX *180 -180/2;
+
+		for(int i=0; i<RANDOM_MAX; i++){
+			for(int j=0; j<cNUM; j++){
+				if(min[i][1]==j){
+					individualsum[j] += angle[i];
+					point_counter[j]++;
+				}
+			}
+		}
+
+		int same_count = 0;
+		for(int i=0;i<cNUM; i++){
+			if(point_counter[i] != 0){
+				if((individualsum[i] / point_counter[i]) == centerpoint[i])
+						same_count ++;
+				
+				centerpoint[i] = individualsum[i] / point_counter[i];
+			}
+
+			if(same_count == cNUM){
+				loop=false;
+			}	
+		}
 	}
 }
 
@@ -130,38 +133,39 @@ void Selection(int angle[], int result[][2])
 {
 	int temp, angle_temp;
 	int target_abs[RANDOM_MAX][2];
-
-	individualData swap;
 	int F=1;
 
-	//クラスタごとに並び替える 000,1111,22,333,4444,555,666666,…
-	while(F){
-		F=0;
-		for(int i=0;i<RANDOM_MAX-1;i++){
-			// 次の個体のクラスタ番号よりも、
-			// 現在の個体のクラスタ番号のほうが高い時
-			if(inv[i].getCluster() > inv[i+1].getCluster() ){
-				// 個体の番号を入れ替え
-				swap		= inv[i];
-				inv[i]		= inv[i+1];
-				inv[i+1]	= swap;
-				
-				F=1;
-			}
-		}
-	}
-
-for(int i=0;i<10;i++){
-		F=1;
-  while(F){
-			F=0;
 
 	for(int i=0; i<RANDOM_MAX; i++)
 	{
 		target_abs[i][0] = std::abs(angle[i] - TARGET_VALUE);
 		target_abs[i][1] = i;
 	}
+	
+	while(F){
+		F=0;
+		for(int j=0; j<RANDOM_MAX-1; j++)
+	{
+		for(int k=j+1; k<RANDOM_MAX; k++)
+		{
+			if(target_abs[j][0] > target_abs[k][0])
+			{
+				temp = target_abs[j][0];
+				angle_temp = target_abs[j][1];
+				target_abs[j][0] = target_abs[k][0];
+				target_abs[j][1] = target_abs[k][1];
+				target_abs[k][0] = temp;
+				target_abs[k][1] = angle_temp;
+			}
+		}
+	}
+	}
 
+	for(int i=0; i<cNUM; i++)
+	{
+		F=1;
+			while(F){
+				F=0;
 	for(int j=0; j<RANDOM_MAX-1; j++)
 	{
 		for(int k=j+1; k<RANDOM_MAX; k++)
@@ -177,6 +181,9 @@ for(int i=0;i<10;i++){
 			}
 		}
 	}
+			}
+	}
+
 
 	for(int l=0; l<RANDOM_MAX * RANKING_RATE; l++)
 	{
@@ -185,8 +192,6 @@ for(int i=0;i<10;i++){
 			result[l][m] = target_abs[l][m];
 		}
 	}
-}
-}
 }
 
 unsigned long DecimalToBinary(std::bitset<32> decimal)
@@ -248,8 +253,8 @@ void Crossover(std::bitset<32> parent[], std::bitset<32> child[])
 	int counter = 0;
 	int pSum=0;
 
-	for(int i=0;i<10;++i){
-
+	for(int j=0; j<cNUM; j++){
+		pSum=0;
 	for(int i=0; i<RANDOM_MAX; i+=2)
 	{	
 		counter += 2;
@@ -282,8 +287,9 @@ void Mutation(std::bitset<32> child[])
 	int mutation_pos;
 	int pSum=0;
 
-	for(int i=0;i<10;++i){
-
+	for(int j=0; j<cNUM; j++)
+	{
+		pSum=0;
 	for(int i=0; i<RANDOM_MAX; i++)
 	{
 		random = Random(0, 100) * 0.01;
@@ -300,6 +306,7 @@ void Mutation(std::bitset<32> child[])
 int main()
 {
 	int angle[RANDOM_MAX];
+	int centerpoint[cNUM];
 	int result[RANDOM_MAX][2];
 	int parent_cpy = 0;
 	std::bitset<32> parent[RANDOM_MAX];
@@ -308,40 +315,9 @@ int main()
 	std::ofstream ofs(GetTimeISOString() + ".csv");
 	
 	Initialize(angle);
-
-	count=0;
-		count2++;
-		if(count2>LOOP_COUNT){
-			//初期化
-			for(int i=0;i<10;i++)inv[i].setRandom();
-			initCentralPoint();
-			count2=0;
-		}
+	CenterInitialize(centerpoint);
+	clustering(angle, centerpoint);
 	
-	clustering(angle);
-
-	int F=1;
-		
-		individualData swap;
-		// ソート済みの個体情報をすべてコピー
-		for(int i=0;i<RANDOM_MAX;i++){
-			invAll[i] = inv[i];
-		}
-
-		// クラスタに関係なく適応度が高い順に並び替える
-		while(F){
-			F=0;
-			for(int i=0;i<RANDOM_MAX-1;i++){
-				// 現個体より次の個体の方が適応度が高かったら入れ替え
-				if(invAll[i].getEva() < invAll[i+1].getEva() ){
-					swap = invAll[i];
-					invAll[i] = invAll[i+1];
-					invAll[i+1] = swap;
-					F=1;
-				}
-			}
-		}
-
 	for(int i=0; i<LOOP_COUNT; i++)
 	{
 		std::cout << "LOOP_COUNT:" << i << std::endl;
@@ -401,8 +377,6 @@ int main()
 		ofs << angle[result[m][1]] << "\t";
 	}
 	ofs << std::endl;
-
-
 
 	return 0;
 }
--- samples/pc/genetic_algorithm/test04/parameter.h
+++ samples/pc/genetic_algorithm/test04/parameter.h
@@ -5,3 +5,4 @@ static const double RANKING_RATE = 0.2;
 static const double INDIVIDUALS_NUMBER = RANDOM_MAX * RANKING_RATE;
 static const double MUTATION_RATE = 0.01;
 static const int MUTATION_POS = 6;
+static const int cNUM=10;
