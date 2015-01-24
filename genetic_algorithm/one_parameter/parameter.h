fa37e8d37348972c03fbd474850121f5b63bb604
Kohei Izumi <kohei.izumi@gmail.com>
2015-01-24 17:55:31 +0900

    Remove one_parameter.

--- genetic_algorithm/one_parameter/parameter.h
+++
@@ -1,7 +0,0 @@
-static const int RANDOM_MAX = 100;
-static const int TARGET_VALUE = 90;
-static const int LOOP_COUNT = 10;
-static const double RANKING_RATE = 0.2;
-static const double INDIVIDUALS_NUMBER = RANDOM_MAX * RANKING_RATE;
-static const double MUTATION_RATE = 0.01;
-static const int MUTATION_POS = 6;
